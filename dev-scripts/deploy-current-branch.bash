6e7313067e3bedc8ca3b8b3ba3c5b9f5b4455a8d
Andres Junge <ajunge@yaykuy.cl>
2019-05-08 11:31:40 -0400

    fix branch deploy

--- dev-scripts/deploy-current-branch.bash
+++ dev-scripts/deploy-current-branch.bash
@@ -1,4 +1,9 @@
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 STAGE=`echo ${CURRENT_BRANCH} | sed 's/\//-/g'`
+
+echo "Config for branch"
+npm run config::branch
+echo "Build"
+npm run build
 echo "Deploying to $STAGE"
 sls deploy -s $STAGE
