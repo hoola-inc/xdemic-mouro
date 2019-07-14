a99631c876f4ba4c93bfc7317f40a24d9d3685a8
Andres Junge <ajunge@yaykuy.cl>
2019-07-14 02:06:43 -0400

    sqlite support

--- src/lib/storageMgr.ts
+++ src/lib/storageMgr.ts
@@ -29,6 +29,7 @@ export class StorageMgr {
     constructor() {
         if(process.env.PG_URL) this.storage = new (require("./pgMgr"))();
         //if(process.env.DYNAMODB_TABLE) this.storage = new (require("./dynamoMgr"))();
+        if(process.env.SQLITE_FILE) this.storage = new (require("./sqliteMgr"))();
         
         //Init Storage
         if(this.storage!=null){
